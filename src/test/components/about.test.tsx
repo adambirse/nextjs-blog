3e7e4b2366d25c85c9d45937a8feba40668220f1
Adam Birse <adambirse@gmail.com>
2021-02-19 14:26:33 +0000

    remove redundant test

--- src/test/components/about.test.tsx
+++ src/test/components/about.test.tsx
@@ -1,16 +1,12 @@
 import * as React from "react";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 import { About } from "../../components/about";
 const chai = require("chai");
 const expect = chai.expect;
 
 describe("About component", () => {
-  it("simple test", function () {
-    expect("About me").to.equal("About me");
-  });
-
   it("should render without throwing an error", function () {
-    const wrap = mount(<About />);
+    const wrap = shallow(<About />);
     expect(wrap.find("p").text()).to.equal("About me");
   });
 });
